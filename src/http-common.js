b1aa8d9cd3365adfb16caaea295f2d5d30076f77
Elvis Campos <lekvispk@gmail.com>
2021-07-03 08:32:22 -0500

    cors

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://hidden-bastion-01869.herokuapp.com",
+  withCredentials: false,
   headers: {
-    "Content-type" : "application/json",
-    "Access-Control-Allow-Origin" : "*"
+    "Content-type" : "application/json" 
   }
 });
